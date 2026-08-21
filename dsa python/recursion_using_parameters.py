94d5226c55bd4644a47ba0dd01d21e9b03a989d6
Sameer Dahiya <sam0dahiya13@gmail.com>
2026-08-21 18:24:39 +0530

    Implement head and tail recursion examples
    Added two new recursive functions: 'otn' to print numbers from 1 to n in head recursion and 'tn' for tail recursion. Also included print statements to differentiate between the two methods.

--- dsa python/recursion_using_parameters.py
+++ dsa python/recursion_using_parameters.py
@@ -6,3 +6,22 @@ def func(x,n):
         func(x,n-1)
 
 func(15,4)
+'''print 1 to n using parameters'''
+'''head method'''
+def otn(n):
+    if n==0:
+        return
+    else:
+        otn(n-1)
+        print(n)
+print("second program")
+otn(7)
+''' tail method'''
+def tn(i,n):
+    if i>n:
+        return
+    else:
+        print(i)
+        tn(i+1,n)
+print("tail method")        
+tn(1,5)
