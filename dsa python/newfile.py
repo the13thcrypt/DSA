59d926aa4aa7b987fcc800a53741cdbc950b0291
Sameer Dahiya <sam0dahiya13@gmail.com>
2026-08-29 17:30:04 +0530

    Update newfile.py

--- dsa python/newfile.py
+++ dsa python/newfile.py
@@ -1,1 +1,24 @@
-print("for commit streak")
+'''s='ANBCDDCBNA'
+#looping
+# pointers 
+n=len(s)
+L=0     
+R=n-1
+while L<R:
+    if s[L]!=s[R]:
+        print("false")
+    L+=1
+    R-=1
+print("true")'''
+s='saaaaaas'
+L=0
+R=len(s)
+#USING RECURSION
+def func(s,L,R):
+    if L>=R:
+        return True
+    if s[L]!=s[R]:
+        return False
+    return func(s,L+1,R-1)
+print(func(s,L,R-1))
+
