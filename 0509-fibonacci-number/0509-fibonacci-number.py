d46e536719906301a995d2e2ad05caf51a7e1bc3
Sameer Dahiya <sam0dahiya13@gmail.com>
2026-08-29 17:53:48 +0530

    Time: 667 ms (14.27%), Space: 12.5 MB (17.23%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def func(self,num):
+        if num==0 or num==1:
+            return num
+        return self.func(num-1)+self.func(num-2)
+    
+    def fib(self, n):
+        answer= self.func(n)
+        return answer
+        """
+        :type n: int
+        :rtype: int
+        """
+        
