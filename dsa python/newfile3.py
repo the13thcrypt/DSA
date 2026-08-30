921b5b85651efe7d4b199848fd023366f55f948f
Sameer Dahiya <samcodes1303@gmail.com>
2026-08-30 16:33:33 +0530

    Implement sorting and reverse sorting functions
    Added functions to sort and reverse sort a list of numbers.

--- dsa python/newfile3.py
+++ dsa python/newfile3.py
@@ -1,5 +1,21 @@
-a=564
-i=0
-while i<=a:
-    print("github commit")
-    i+=i
+nums=[4,5,6,7,3,4,2,4,7,5]
+def sortfunc(nums):
+    n=len(nums)
+    for i in range(0,n):
+        min_index=i
+        for j in range(i+1,n):
+            if nums[j]<nums[min_index]:
+                min_index=j
+        nums[i],nums[min_index]=nums[min_index],nums[i]
+sortfunc(nums)
+print(nums)            
+def reverse_sort(nums):
+    n=len(nums)
+    for i in range(0,n):
+        min_index=i
+        for j in range(i+1,n):
+            if nums[j]>nums[min_index]:
+                min_index=j
+        nums[i],nums[min_index]=nums[min_index],nums[i]
+reverse_sort(nums)
+print(nums)
