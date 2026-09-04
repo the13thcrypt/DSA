2bcabccfc582da55b3bd52ef4ca4b10bd94eb33e
Sameer Dahiya <sam0dahiya13@gmail.com>
2026-09-05 00:08:58 +0530

    Implement brute force solution for two sum problem
    Refactor twoSum method to use brute force approach.

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,4 +4,11 @@ class Solution:
             need=target-nums[i]
             if(need in nums and nums.index(need)!=i):
                 return [i,nums.index(need)]
-                
+#brute force
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        for i in range(0,n):
+            for j in range(i+1,n):
+                if nums[i]+nums[j]==target:
+                    return[i,j]
