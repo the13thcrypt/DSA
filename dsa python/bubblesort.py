a85cc1013308cb18f320ad97be48ebe5493fc78e
Sameer Dahiya <sam0dahiya13@gmail.com>
2026-09-04 23:25:07 +0530

    Document time and space complexity of bubble sort
    Added time and space complexity analysis for bubble sort.

--- dsa python/bubblesort.py
+++ dsa python/bubblesort.py
@@ -8,4 +8,4 @@ def bubble_sort(num):
                 num[j],num[j+1]=num[j+1],num[j]
 bubble_sort(nums)
 print(nums)
-            
+            ##time complexity of bubble sort is O(n^2) in worst and average case, and O(n) in best case. The space complexity of bubble sort is O(1) because it only requires a constant amount of additional memory space for the temporary variable used for swapping. */
