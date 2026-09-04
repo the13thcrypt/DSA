7ab52de8a4ac15b059af93b67e647e9bb603f957
Sameer Dahiya <sam0dahiya13@gmail.com>
2026-09-04 23:23:13 +0530

    Add files via upload

---
+++ dsa python/bubblesort.py
@@ -0,0 +1,11 @@
+# bubble sort algorithm in Python is also called Adjacent swap / adjacent pair sorting algorithm. It is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted, comparing each pair of adjacent items and swapping them if they are in the wrong order. The pass through the list is repeated until no swaps are needed, which means the list is sorted.
+nums=[64, 34, 25, 12, 22, 11, 90]
+def bubble_sort(num):
+    n=len(num)
+    for i in range(n-2,-1,-1):
+        for j in range(0,i+1):
+            if num[j]>num[j+1]:
+                num[j],num[j+1]=num[j+1],num[j]
+bubble_sort(nums)
+print(nums)
+            
