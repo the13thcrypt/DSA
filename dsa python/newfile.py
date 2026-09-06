8262de1b4ac9ef81ee4c616d37235fd3f8d21381
Sameer Dahiya <sam0dahiya13@gmail.com>
2026-09-06 23:11:41 +0530

    Implement merge sort function in newfile.py

--- dsa python/newfile.py
+++ dsa python/newfile.py
@@ -1,5 +1,14 @@
-def commit(a):
-    print(a)
-commit('sam')
-commit('this is for commiting')
-# this is only for streak a
+nums=[3,2,1,4,5,3,2,1,2,6,4,2,1,7,11]
+def merge_sort(nums):
+    result=[]
+    i,j=0
+    n,m=len(left),len(right)
+    while i<n and j<m:
+        if left[i]<right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    if i<n:
+        while    
